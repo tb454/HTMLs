00558e6689de5dfb0a678bf74b1540ff0954fda7
Tony <tbyers1233@gmailcom>
2025-02-26 16:35:38 -0800

    modified:   bridge-dashboard/server.js

--- bridge-dashboard/server.js
+++ bridge-dashboard/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000; // We'll use port 3000
+const port = 3001; // We'll use port 3001
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
