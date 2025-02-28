488f53379ec6805269b27b84ef5525e66021acdc
Tony <tbyers1233@gmailcom>
2025-02-27 16:22:26 -0800

    modified:   bridge-dashboard/server.js

--- bridge-dashboard/server.js
+++ bridge-dashboard/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = 4000;
+const port = 4001;
 app.listen(port, () => {
   console.log(`🔥 Server running on http://localhost:${port}`);
 });
